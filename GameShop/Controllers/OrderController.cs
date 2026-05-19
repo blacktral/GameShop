71e84228e3baf4295098490126557b95b73a24df
blacktral <prohodeckirilo@gmail.com>
2026-05-19 19:13:55 +0300

    gfdggfdg

--- GameShop/Controllers/OrderController.cs
+++ GameShop/Controllers/OrderController.cs
@@ -104,7 +104,7 @@ namespace GameShop.Controllers
             _context.SaveChanges();
 
             // --- ЕТАП 3: Фінал ---
-            _cartService.ClearCart(); // Очищаємо кошик ffdsfdsf
+            _cartService.ClearCart(); // Очищаємо кошик ffdsfdsf рпрпрпрп
 
             return RedirectToAction("CheckoutComplete");
         }
